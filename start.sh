6d700f660bfac4b0ff0493312f2a3bb614c8c217
Jason Sicotte <sicotte.jason@gmail.com>
2025-08-25 17:35:27 -0500

    removing old code

--- start.sh
+++ start.sh
@@ -1,32 +1,3 @@
-# qemu-system-aarch64 \
-#   -machine virt,accel=hvf \
-#   -cpu cortex-a72 -smp 4 -m 4G \
-#   -device virtio-gpu-pci \
-#   -display default,show-cursor=on \
-#   -device usb-ehci,id=usb \
-#   -usb -device usb-tablet \
-#   -device virtio-keyboard-pci \
-#   -drive "format=raw,file=edk2-aarch64-code.fd,if=pflash,readonly=on" \
-#   -drive "format=raw,file=ovmf_vars.fd,if=pflash" \
-#   -drive "format=qcow2,file=virtual-disk.qcow2" \
-#   -cdrom Fedora-Workstation-Live-42-1.1.aarch64.iso
-
-# this one works I got most of the config from https://en.opensuse.org/openSUSE:AArch64#Using_an_emulator
-  # qemu-system-aarch64 \
-  #   -machine virt,accel=hvf \
-  #   -m 4G \
-  #   -cpu cortex-a72 \
-  #   -smp 4 \
-  #   -M virt \
-  #   -bios edk2-aarch64-code.fd \
-  #   -device virtio-gpu-pci \
-  #   -display default,show-cursor=on \
-  #   -device nec-usb-xhci \
-  #   -device usb-tablet \
-  #   -device usb-kbd \
-  #   -drive "format=qcow2,file=virtual-disk.qcow2" \
-  #   -cdrom Fedora-Workstation-Live-42-1.1.aarch64.iso
-
   # this one works I got most of the config from https://en.opensuse.org/openSUSE:AArch64#Using_an_emulator
   qemu-system-aarch64 \
     -machine virt,accel=hvf \
@@ -40,5 +11,5 @@
     -device nec-usb-xhci \
     -device usb-tablet \
     -device usb-kbd \
-    -drive "format=qcow2,file=virtual-disk.qcow2" \
-    -cdrom Fedora-Workstation-Live-42-1.1.aarch64.iso
+    -drive "format=qcow2,file=virtual-disk.qcow2" #\
+    #-cdrom Fedora-Workstation-Live-42-1.1.aarch64.iso
